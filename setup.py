ec0a83fd2f9a082ad35c2c8eb45c1a3b0836d838
Zachariah Reed <zreed@fastmail.com>
2017-02-10 15:15:27 -0800

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import sys
 from setuptools import setup
 
 __author__  = 'Zachariah Reed'
-__version__ = '0.1'
+__version__ = '0.2'
 __contact__ = 'zreed@fastmail.com'
 __url__     = 'https://github.com/zachariahreed/byteasm'
 __license__ = 'GPL'
 
-if sys.version_info < (3,4) :
-  raise NotImplementedError( 'byteasm requires Python 3.4+' )
+if sys.version_info < (3,6) :
+  raise NotImplementedError( 'byteasm requires Python 3.6+' )
 
 setup(
     name         = 'byteasm'
@@ -26,7 +26,6 @@ setup(
                       , 'Intended Audience :: Developers'
                       , 'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)'
                       , 'Programming Language :: Python :: 3 :: Only'
-                      , 'Programming Language :: Python :: 3.4'
-                      , 'Programming Language :: Python :: 3.5'
+                      , 'Programming Language :: Python :: 3.6'
                       ]
   )
